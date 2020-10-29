5262defe2f91950d373e305cf86eb03a26851195
Martin LindstrÃ¶m <martin.lindstrom@litsec.se>
2020-10-29 15:16:51 +0100

    Fixes for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,8 +12,9 @@ docker rm signservice-integration-rest --force
 
 echo Redeploying docker container signservice-integration-rest ...
 docker run -d --name signservice-integration-rest --restart=always \
-  -p 9050:8443 \
-  -p 9056:8009 \
+  -p 9060:8443 \
+  -p 9069:8009 \
+  -e SERVER_SERVLET_CONTEXT_PATH=/signint \
   -e "SPRING_CONFIG_ADDITIONAL_LOCATION=/opt/signservice-integration-rest/" \
   -v /etc/localtime:/etc/localtime:ro \
   -v /opt/docker/signservice-integration-rest:/opt/signservice-integration-rest \
